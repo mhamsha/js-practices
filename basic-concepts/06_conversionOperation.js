01622191241b86469b01a5b9a04eb713f6b3094d
Hamza Shahzad <hamza.shahzad.8692@gamail.com>
2023-10-14 11:31:15 +0500

    operations

--- basic-concepts/06_conversionOperation.js
+++ basic-concepts/06_conversionOperation.js
@@ -39,19 +39,64 @@
 
 // // undefined => NaN ; null => 0; if we convert them into number this will happen
 
-let scoreString = 33;
-console.log(scoreString);
-console.log(typeof(scoreString));
-let scoreNumber = String(scoreString);
-console.log(typeof scoreNumber);
-console.log(scoreNumber);
-console.log(scoreNumber + 10);
+// let scoreString = 33;
+// console.log(scoreString);
+// console.log(typeof(scoreString));
+// let scoreNumber = String(scoreString);
+// console.log(typeof scoreNumber);
+// console.log(scoreNumber);
+// console.log(scoreNumber + 10);
 
 // // we can also make number into string so we cannot use arithmetic operations on them;
 
 //==-------------------------Operations-------------------------------------==//
 
-let value = 10;
-let negValue = -value
-console.log(value);
-console.log(negValue);
+// let value = 10;
+// let negValue = -value
+// console.log(value);
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2/2);
+
+// let str1 = "hamza "
+// let str2 = "shahzad "
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 1)
+// console.log(1 + "1")
+// console.log("1" + "1")
+// console.log(1 + 1)
+
+// console.log("1" + 1 + 1)
+// console.log(1 + "1" + 1)
+// console.log(1 + 1 + "1")
+// console.log("1" + "1" + 1)
+// console.log(1 + "1" +"1")
+// console.log("1" + "1" + "1")
+// console.log(1 + 1 + 1)
+
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter)
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter)
+
+
+// console.log(+true)
+// console.log(true+) //error
+// console.log(+false)
+
+console.log(+"")
+
+let num1 ,num2 , num3
+num1 = num2 = num3 = 2+2
+console.log([num1, num2, num3])
+
+console.log(1 + 2 - 3 * 4 / 3 % 2 )
