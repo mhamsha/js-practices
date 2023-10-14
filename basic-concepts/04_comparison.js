06771c2cf71f34b60c5764e871a264cebf713d7a
Hamza Shahzad <hamza.shahzad.8692@gamail.com>
2023-10-14 12:25:37 +0500

    comparisons

---
+++ basic-concepts/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2==2) // it will convert the data type if needed
+// console.log(2>2) // it will also convert 
+// console.log(2<2) // it will  convert too
+// console.log(2>=2) // it'll also convert
+// console.log(2<=2) // wi'll also convert
+// console.log(2!=2) //it will also convert
+// console.log(2===2) // it won't convert the data type and check strictly
+// console.log(2!==2) //it won't also convert
+
+// console.log("2" >= 1)
+
+console.log(null >= 1) 
+console.log(null == 1)
+console.log(null <= 1)
+
+// equality operators and comparisons work differently 
+//  like comparisons change null into 0 and then compare so we got.
+// but in equality case it does'nt convert into 0 but to NaN
+
+console.log(undefined >= 1) 
+console.log(undefined == 1)
+console.log(undefined <= 1)
