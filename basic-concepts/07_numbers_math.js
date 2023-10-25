c154464ce80a341ee2a32bb24abde90435fd94cb
Hamza Shahzad <hamza.shahzad.8692@gamail.com>
2023-10-25 21:54:23 +0500

    numbers

---
+++ basic-concepts/07_numbers_math.js
@@ -0,0 +1,16 @@
+// * Number and Maths in JavaScript
+// ? Numbers:
+// * First way
+const myNumber = 123;
+// console.log(myNumber);
+// * Second way
+const mySecondNum = new Number(10000000)
+console.log(mySecondNum);
+// ? Methods:
+// const a = mySecondNum.toString().length
+// console.log(a)
+// console.log(typeof a);
+// console.log(mySecondNum.toFixed(3));
+// console.log(mySecondNum.toPrecision(14));
+// console.log(mySecondNum.toLocaleString());
+console.log(mySecondNum.MIN_VALUE);
