7b901dbc43fe3ad45cb7c05508abf1aed31371fa
Hamza Shahzad <hamza.shahzad.8692@gamail.com>
2023-10-26 16:12:07 +0500

    math

--- basic-concepts/07_numbers_math.js
+++ basic-concepts/07_numbers_math.js
@@ -5,7 +5,7 @@ const myNumber = 123;
 // console.log(myNumber);
 // * Second way
 const mySecondNum = new Number(10000000)
-console.log(mySecondNum);
+// console.log(mySecondNum);
 // ? Methods:
 // const a = mySecondNum.toString().length
 // console.log(a)
@@ -13,4 +13,20 @@ console.log(mySecondNum);
 // console.log(mySecondNum.toFixed(3));
 // console.log(mySecondNum.toPrecision(14));
 // console.log(mySecondNum.toLocaleString());
-console.log(mySecondNum.MIN_VALUE);
+// console.log(mySecondNum.MIN_VALUE);
+// ? +++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++
+// console.log(Math.E);
+// console.log(Math.abs(-234235));
+// console.log(Math.floor(2.9));
+// console.log(Math.ceil(2.9));
+// console.log(Math.round(3.9));
+// console.log(Math.min(124,456234,4234532452,24));
+// console.log(Math.max(124,456234,4234532452,24));
+// console.log(Math.sqrt(2));
+// console.log(Math.pow(2,3));
+// console.log(Math.floor((Math.random() *  3 ) + 1  ) );
+
+// const minValue = 10;
+// const maxValue = 20;
+// console.log(Math.floor(Math.random() * ((maxValue - minValue) +1)) + minValue) 
+
